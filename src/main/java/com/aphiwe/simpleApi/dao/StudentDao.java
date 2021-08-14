2fe7db1122c4eab41fc3fb2997b0b5fe009d4b60
aphiwe <aphiwe_b@outkook.com>
2021-08-14 05:04:07 +0200

    Add mysql database connection

--- src/main/java/com/aphiwe/simpleApi/dao/StudentDao.java
+++ src/main/java/com/aphiwe/simpleApi/dao/StudentDao.java
@@ -1,4 +1,4 @@
-package com.aphiwe.simpleApi.service;
+package com.aphiwe.simpleApi.dao;
 
 import com.aphiwe.simpleApi.model.Student;
 
@@ -8,6 +8,7 @@ import java.util.Optional;
 public interface StudentDao {
     public int save(Student student);
     public List<Student> fetchStudents();
-    public int updateStudent(int id, Student student);
+    public int updateStudent(Student student);
+    public int deleteStudent(int id);
     public Optional<Student> findById(int id);
 }
--- src/main/java/com/aphiwe/simpleApi/service/StudentService.java
+++ src/main/java/com/aphiwe/simpleApi/service/StudentService.java
@@ -1,45 +1,81 @@
 package com.aphiwe.simpleApi.service;
 
+import com.aphiwe.simpleApi.dao.StudentDao;
 import com.aphiwe.simpleApi.model.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class StudentService implements StudentDao {
-    private final List<Student> students = new ArrayList<>();
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    Logger logger = LoggerFactory.getLogger(StudentService.class);
 
     @Override
     public int save(Student student) {
-        if (students.contains(student)) {
-            return 204;
+        String sql = "INSERT INTO students (id, name, gpa) VALUES (?, ?, ?)";
+        int result = jdbcTemplate.update(sql, student.getId(), student.getName(), student.getGpa());
+        if (result > 0) {
+            logger.info("new student added");
         }
-        students.add(student);
         return 204;
     }
 
     @Override
     public List<Student> fetchStudents() {
-        if (students.isEmpty()) {
-            students.add(new Student(1, "Aphiwe", 45.5));
-        }
+        String sql = "SELECT * FROM students";
+        List<Student> students = jdbcTemplate.query(
+                sql,
+                new BeanPropertyRowMapper(Student.class));
         return students;
     }
 
     @Override
-    public int updateStudent(int id, Student student) {
-        if(findById(student.getId()).isPresent() && id==student.getId()){
-             students.remove(findById(id).get());
-             students.add(student);
-           };
+    public int updateStudent(Student student) {
+        String sql = "UPDATE students  set name = ?, gpa = ? where id = ?";
+        int result = jdbcTemplate.update(sql, student.getName(), student.getGpa(), student.getId());
+        if (result > 0) {
+            logger.info("student updated.");
+            return 204;
+        } else {
+            return save(student);
+        }
+    }
+    @Override
+    public int deleteStudent(int id) {
+        String sql = "DELETE FROM students WHERE id = ?";
+        int result = jdbcTemplate.update(sql, id);
+        if (result > 0) {
+            logger.info("student deleted.");
+        }
         return 204;
     }
 
     @Override
     public Optional<Student> findById(int id) {
-        return students.stream().filter(student1 -> student1.getId()==id).findFirst();
+        try {
+            String sql = "SELECT * FROM students WHERE id = ?";
+            return Optional.ofNullable(
+                    (Student)  jdbcTemplate
+                            .queryForObject(
+                                    sql,
+                                    new Object[]{id},
+                                    new BeanPropertyRowMapper(Student.class)
+                            ));
+        } catch (EmptyResultDataAccessException ex){
+            logger.info("No student found with id " + id);
+            return null;
+        }
     }
 
 }
